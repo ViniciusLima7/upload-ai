9af83bc89fab49d34d23e99ce641ad719a7356cd
Vinicius Lima <marcos.vf@samsung.com>
2023-09-11 22:07:21 -0300

    feat: add libs tailwind postcss radix ui and shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <h1>Hello World IA</h1>;
+  return (
+    <>
+      <h1>Hello World IA</h1>
+      <Button>Hello World IA</Button>
+    </>
+  );
 }
