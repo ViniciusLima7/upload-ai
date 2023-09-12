9aec4910fa6fb8269b7386182d8b31c8cf9b35a4
Vinicius Lima <marcos.vf@samsung.com>
2023-09-11 22:24:32 -0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
 
 export function App() {
   return (
-    <>
-      <h1>Hello World IA</h1>
-      <Button>Hello World IA</Button>
-    </>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between  border-b">
+        <h1 className="text-xl font-bold">Upload IA</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW IA por Vinicius Lima
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            GitHub
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
